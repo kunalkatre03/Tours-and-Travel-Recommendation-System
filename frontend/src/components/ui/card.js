0f988c573d2d2cce98b18839eb3aa29b53b3da36
shashwatpratapsingh7717 <shashwatsingh7717@gmail.com>
2025-02-21 22:34:47 +0530

    deleted the frontend from backend folder, because it already exists outside the backend folder

--- frontend/src/components/ui/card.js
+++
@@ -1,9 +0,0 @@
-export function Card({ title, children }) {
-    return (
-      <div className="border shadow-md p-4 rounded-lg bg-white">
-        <h2 className="text-lg font-bold">{title}</h2>
-        <div>{children}</div>
-      </div>
-    );
-  }
-  
