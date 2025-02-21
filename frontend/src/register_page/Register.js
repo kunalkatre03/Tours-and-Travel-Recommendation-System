57bda6a078b09fb69d7b643c565b197b12c1e2fb
shashwatpratapsingh7717 <shashwatsingh7717@gmail.com>
2025-02-21 23:07:01 +0530

    created register page

---
+++ frontend/src/register_page/Register.js
@@ -0,0 +1,4 @@
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from reloading the page
+    alert("Registration Successful! Welcome to your travel journey!");
+});
